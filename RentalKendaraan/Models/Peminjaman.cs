d494687a05a3b079a011cdc0acb33af9d944a118
Bramantyoadi12 <56532145+Bramantyoadi12@users.noreply.github.com>
2020-12-08 02:04:02 +0700

    First Commit

---
+++ RentalKendaraan/Models/Peminjaman.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace RentalKendaraan_20180140052.Models
+{
+    public partial class Peminjaman
+    {
+        public int IdPeminjaman { get; set; }
+
+        [Required(ErrorMessage = "Tanggal Peminjaman tidak boleh kosong")]
+        public DateTime? TglPeminjaman { get; set; }
+        public int? IdKendaraan { get; set; }
+        public int? IdCostumer { get; set; }
+        public int? IdJaminan { get; set; }
+        public int? Biaya { get; set; }
+
+        public Customer IdCostumerNavigation { get; set; }
+        public Jaminan IdJaminanNavigation { get; set; }
+        public Kendaraan IdKendaraanNavigation { get; set; }
+        public Peminjaman IdPeminjamanNavigation { get; set; }
+        public Peminjaman InverseIdPeminjamanNavigation { get; set; }
+    }
+}
